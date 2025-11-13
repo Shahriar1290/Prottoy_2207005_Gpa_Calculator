acb950cf0280c7e689941e22aab164c0e0c573b8
Shahriar Hossain Prottoy <shahriarh346@gmail.com>
2025-11-14 00:46:54 +0600

    Initial Commit

---
+++ src/main/java/com/example/prottoy_2207005_gpa_calculator/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.prottoy_2207005_gpa_calculator;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
